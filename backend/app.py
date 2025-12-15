a6bacad8178f3bde013614c9507aaa8b19c8bbf9
Tonttu81 <joni.pylkkonen@gmail.com>
2025-12-15 23:11:12 +0200

    Testing

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ def get_db_connection():
 
 @app.route('/api/health')
 def health():
-    return jsonify({"status": "healthy"})
+    return jsonify({"status": "healthy " + os.getenv('DB_NAME') + " " + os.getenv('DB_USER')})
 
 @app.route('/api/users')
 def get_users():
